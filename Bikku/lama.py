f487c3e134f9f90b05bce1675e5e29ac77630e0a
bikrajlama <bxl038@shsu.edu>
2021-02-24 10:52:31 -0600

    bnjnj

--- Bikku/lama.py
+++ Bikku/lama.py
@@ -1,2 +1,15 @@
-print("hello world151")
-print ("MALL")
+class Robot:
+    def __init__(self, name, color, weight):
+        self.name = name
+        self.color = color
+        self.weight = weight
+
+    def introduce_self(self):
+        print("My name is "+ self.name)
+
+r1 = Robot("Tom", "red", 30)
+r2 = Robot("Jerry", "Blue", 40)
+
+r1.introduce_self()
+r2.introduce_self()
+
